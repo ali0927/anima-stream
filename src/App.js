c092a28c04b2440bdf66947793701bb007998286
ali0927 <kevinali0927@gmail.com>
2022-09-29 19:06:24 +0400

    initial version do not build

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ import { Planet } from "./components/planets/planet/planet";
 import "@fontsource/inter";
 import "./index.scss";
 import "./global.scss";
+import { Rooms } from "./components/planets/planet/rooms/rooms";
 
 function App() {
   const [signer, setSigner] = useState();
@@ -82,6 +83,7 @@ function App() {
             <Route exact path="/charge" component={Charge} />
             <Route exact path="/planets" component={PlanetsCarousel} />
             <Route path={"/planets/:title"} component={Planet} />
+            <Route path={"/planets/:title/rooms"} component={Rooms} />
 
             <Route exact path="/login">
               <WalletConnect />
